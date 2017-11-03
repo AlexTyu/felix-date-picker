0c624693a0bb63fd2c2047a2790df0f2cfacb573
Sergey Samusenko <sergey.samusenko@inpwrd.com>
2017-11-02 21:12:49 -0700

    init npm

--- ng-daterangepicker.module.ts
+++ ng-daterangepicker.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { NgDaterangepickerComponent } from './ng-daterangepicker/ng-daterangepicker.component';
 import { ToWeekdayPipe } from './to-weekday.pipe';
 import { SplitByWeeksPipe } from './split-by-weeks.pipe';
